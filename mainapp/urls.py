7bec042d7d029b1cd2ef5f592ba0b819c15080c1
Egor <yegor.rabota.gu@mail.ru>
2021-12-27 18:36:11 +0300

    fixed urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,6 +3,6 @@ from mainapp.views import index, audio, AudioDownloadListView
 
 urlpatterns = [
     path('', index, name='index'),
-    path('audio', audio, name='audio'),
-    path('audio/download', AudioDownloadListView.as_view(), name='audio_converted'),
+    path('audio/', audio, name='audio'),
+    path('audio/download/', AudioDownloadListView.as_view(), name='audio_converted'),
 ]
